2ddb920705f06de43a70d99baec9afb5d2c3167d
Hetty de Vries <hetty82@gmail.com>
2016-11-29 18:41:33 +0100

    Add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,17 @@ const gulp          = require('gulp'),
       browserify    = require('browserify'),
       // streamify turns it back into a stream (after source unstreamed it)
       streamify     = require('gulp-streamify'),
-      uglify        = require('gulp-uglify');
+      uglify        = require('gulp-uglify'),
 
-let env = process.env.NODE_ENV || 'development';
+      sass          = require('gulp-sass');
+
+let env             = process.env.NODE_ENV || 'development';
+let outputDir       = './dist'
+
+
+// -----
+// TASKS
+// -----
 
 // create a default task and just log a message
 gulp.task('default', function() {
@@ -33,5 +41,22 @@ gulp.task('scripts', function () {
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulpif(env === 'production', streamify(uglify())))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(outputDir));
+});
+
+// process styles
+gulp.task('styles', function() {
+  let config = {};
+
+  if (env === 'development') {
+    config.sourceComments = 'map';
+  }
+
+  if (env === 'production') {
+    config.outputStyle = 'compressed';
+  }
+
+  return gulp.src('src/styles/app.sass')
+    .pipe(sass(config))
+    .pipe(gulp.dest(outputDir))
 });
